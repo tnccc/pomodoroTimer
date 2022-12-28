2c5b592686a88fbe8de220d25d57dc50d9b34b3b
tnccc <irei.frontend@gmail.com>
2022-12-28 13:43:08 +0900

    PR作成のためにソースコードをコピーする

---
+++ components_pr/SoundPlayer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    :class="$style.wrapper"
+  >
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SoundPlayer',
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" module>
+@use "@/assets/scss/value" as v;
+@use "@/assets/scss/font" as f;
+@use "@/assets/scss/color" as c;
+@use "@/assets/scss/mixin" as m;
+
+</style>
