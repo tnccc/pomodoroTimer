2f94660b098696a2db5b73ae9dac526f8851f2b0
tnccc <irei.frontend@gmail.com>
2023-01-15 17:23:49 +0900

    feat: #4 TodoItem内にデリートボタンを実装する

--- components/TodoList.vue
+++ components/TodoList.vue
@@ -1,121 +1,29 @@
 <template>
   <div
-    :class="$style.wrapper"
+    :class="$style.container"
   >
-    <div
-      :class="$style.container"
-    >
-      <div
-        :class="$style.heading_container"
-      >
-        <h2
-          :class="$style.heading"
-        >
-          Tasks
-          <!-- タスクを追加するとタスク数が分かるようにしたい -->
-          <span :class="$style.num">{{ itemLength }}</span>
-        </h2>
-        <button
-          :class="$style.heading_button"
-        >
-          <img src="@/assets/images/task_menu.svg" alt="taskmenu">
-        </button>
-      </div>
-      <div
-        :class="$style.todo"
-      >
-        <div
-          :class="$style.todo_list"
-        >
-          <TodoItem
-            v-if="addTaskDisplay"
-            :type="'task'"
-            v-model="todoItem"
-          >
-            <span
-              @click="deleteTodo"
-              :class="$style.todo_task"
-            />
-          </TodoItem>
-          <TodoItem
-            v-if="taskDisplay"
-            :type="'default'"
-            @onClick="taskDisplay = !taskDisplay"
-            v-for="(item, key) in todos"
-            :key="key"
-            :class="$style.todo_item"
-          >
-            {{ item.task }}
-          </TodoItem>
-          <TodoItemAdd
-            v-else
-          >
-            <CommonButton
-              @onClick="addTodo"
-              :btnText="'Save'"
-            >
-            </CommonButton>
-            <CommonButton
-              @onClick="taskDisplay = !taskDisplay"
-              :btnText="'Cancel'"
-            >
-            </CommonButton>
-          </TodoItemAdd>
-          
-        </div>
-      </div>
-    </div>
-    <!-- sample -->
-    <div class="sample">
-        <textarea
-          type="textarea"
-          placeholder="簡単な説明を入力しましょう。"
-          v-model="sampleText"
-        ></textarea>
-        <button @click="addTodoSample">sampleAdd</button>
-    </div>
+    <TodoItem 
+      v-for="(todo, index) in todos"
+      :key="index"
+      :todo="todo"
+      @deleteButtonClick="deleteTodo"
+    />
   </div>
 </template>
 
 <script>
-import {mapActions, mapGetters} from 'vuex';
 
 export default {
   name: 'TodoList',
-  data() {
-    return {
-      sampleText: '',
-      todoItem: "",
-      itemLength: 0,
-      taskDisplay: true,
-      addTaskDisplay: false,
-    }
-  },
-  computed: {
-    ...mapGetters({
-      todos: 'todo/todoList'
-    })
+  props: {
+    todos: {
+      required: true,
+      type    : Array,
+    },
   },
   methods: {
-    ...mapActions({
-      addTodoItem: 'todo/add',
-      deleteItem: 'todo/delete'
-    }),
-    addTodo(text) {
-      console.log('before call action=>',text);
-      // this.addTodoItem(this.todoItem)
-      this.addTodoItem(text)
-      this.todoItem = ""
-      this.taskDisplay = true
-      this.addTaskDisplay = true
-    },
-    addTodoSample() {
-      this.addTodoItem(this.sampleText)
-      this.sampleText = ""
-    },
-    deleteTodo(key) {
-      console.log('fire')
-      this.deleteItem(key)
+    deleteTodo(todo) {
+      this.$emit('deleteTodoStart', todo)
     },
   },
 }
@@ -127,60 +35,8 @@ export default {
 @use "@/assets/scss/color" as c;
 @use "@/assets/scss/mixin" as m;
 
-.wrapper {
-  min-width: 300px;
-  border-left: solid 2px c.$lightGray;
-}
-
 .container {
-  padding: 0 v.clampFunc(20, 24, 32, 1024);
-}
-
-.heading {
-  font-weight: bolder;
-
-  &_container {
-    display              : grid;
-    grid-template-columns: 2fr 1fr;
-  }
-
-  &_button {
-    margin-left: auto;
-    width      : 2rem;
-  }
-}
-
-.todo {
-    &_list {
-      margin-top : calc(var(--sv) * 2.5);
-      display    : grid;
-      place-items: start;
-      gap        : calc(var(--sv) * 3) 0;
-    }
-
-    &_task {
-      position        : relative;
-      width           : calc(var(--sv) * 2.5);
-      height          : calc(var(--sv) * 2.5);
-      border          : solid 1px c.$gray;
-      border-radius   : 50%;
-      background-color: transparent;
-      pointer-events  : auto;
-      
-      &:hover {
-
-        &:before {
-          content      : "";
-          position     : absolute;
-          top          : 4px;
-          left         : 3px;
-          width        : 12px;
-          height       : 7px;
-          border-left  : 3px solid c.$mainGreen;
-          border-bottom: 3px solid c.$mainGreen;
-          transform    : rotate(-45deg);
-        }
-      }
-    }
+  display: grid;
+  gap: calc(var(--bv) * 2) 0;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -13,39 +13,218 @@
           <PomodoroTimer 
             :class="$style.timer"
           />
-          <TodoList 
-            :class="$style.todo"
-          />
+          <div
+            :class="$style.todo_wrapper"
+          >
+            <div
+              :class="$style.todo"
+            >
+              <div
+                :class="$style.todo_heading_wrapper"
+              >
+                <h2
+                  :class="$style.todo_heading"
+                >
+                  Tasks
+                <span :class="$style.num">
+                  {{ todos.length  }}
+                </span>
+                </h2>
+                <button
+                  :class="$style.todo_heading_button"
+                >
+                  <img src="@/assets/images/task_menu.svg" alt="taskmenu">
+                </button>
+              </div>
+              <div
+                :class="$style.todo_content"
+              >
+                <div
+                  :class="$style.todo_list_container"
+                >
+                  <TodoList 
+                    @deleteTodoStart="deleteTodoStart"
+                    :todos="todos"
+                  />
+                </div>
+                <div>
+                  <AddTodo 
+                    v-if="todoAddMode" 
+                    @saveButtonClick="addTodo"
+                    @cancelButtonClick="cancelButtonClick"
+                    :class="$style.todo_add"
+                  />
+                  <!-- タスク一覧下部に固定配置 -->
+                  <button
+                    v-if="!todoAddMode" @click="startAddMode"
+                    :class="$style.todo_input"
+                  > 
+                    タスクを入力をしてください。
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </main>
+    <SoundPlayer
+      :class="$style.sound"
+    />
   </div>
 </template>
 
 <script>
+import {mapActions, mapGetters} from 'vuex';
+
 export default {
   name: 'Top-Page',
+  data() {
+    return {
+      todoAddMode: false,
+      text       : '',
+    }
+  },
+  computed: {
+    ...mapGetters({
+      todos: 'todo/todoList'
+    }),
+  },
+  methods: {
+    ...mapActions({
+      addTodoItem: 'todo/add',
+      deleteItem: 'todo/delete'
+    }),
+    addTodo(text) {
+      this.text = text
+      if(!text || text.trim().length === 0) {
+        this.todoAddMode = false
+      } else {
+        console.log('=> testClick', text)
+        this.addTodoItem(text)
+        this.todoAddMode = false
+      }
+    },
+    deleteTodoStart(todo) {
+      this.deleteItem(todo)
+    },
+    cancelButtonClick() {
+      this.todoAddMode = false
+    },
+    startAddMode() {
+      this.todoAddMode = true
+    },
+
+  },
 }
 </script>
 
 <style lang="scss" module>
 @use "@/assets/scss/value" as v;
 @use "@/assets/scss/font" as f;
+@use "@/assets/scss/color" as c;
+@use "@/assets/scss/mixin" as m;
 
 .container {
-  width: 100%;
-  --sv : .5rem;
-  --black: #202124;
-  --light-gray: #E2E8F0;
-  --accent-color: #0B57D0;
+  width           : 100%;
+  --bv            : .5rem;
+  --white         : #fff;
+  --black         : #202124;
+  --gray          : #5F6368;
+  --light-gray    : #E2E8F0;
+  --dull-gray     : #CBD5E1;
+  --accent-color  : #0B57D0;
 }
 
 .contents {
-  margin-top: v.viewMargin(1400, 64);
+  margin-top       : v.viewMargin(1400, 40);
+  scroll-snap-align: start;
 
   &_grid {
-    display: grid;
-    grid-template-columns: 70% 30%;
+    display: flex;
   }
+}
+
+.timer {
+  flex  : 1 0 auto;
+  height: calc(100vh - 45px);
+}
+
+.todo {
+  padding       : 0 v.clampFunc(20, 24, 32, 1024);
+  display       : flex;
+  flex-direction: column;
+  
+  &_wrapper {
+    flex       : 0 0 30%;
+    border-left: solid 2px var(--light-gray);
+  }
+
+  &_content {
+    position  : relative;
+    margin-top: calc(var(--bv) * 2);
+    width     : 100%;
+  }
+
+  &_heading {
+    font-weight: bolder;
+
+    .num {
+      margin-left: var((--bv));
+    }
+
+    &_wrapper {
+      display              : grid;
+      width                : 100%;
+      grid-template-columns: 2fr 1fr;
+    }
+
+    &_button {
+      margin-left: auto;
+      width      : 2rem;
+    }
+  }
+
+  &_input {
+    margin-top   : calc(var(--bv) * 2);
+    padding      : v.clampFunc(12, 16, 16, 1024);
+    width        : 100%;
+    display      : flex;
+    align-items  : center;
+    font-size    : v.clampFunc(14, 15, 16, 1024);
+    color        : c.$gray;
+    gap          : 0 var(--bv);
+    border       : dashed 2px var(--dull-gray);
+    border-radius: 12px;
+    box-shadow   : inset 0 0 14px 3px rgba(0 0 0 / .1);
+
+    &::before {
+      content         : "";
+      display         : block;
+      width           : 13px;
+      height          : 2px;
+      background-color: var(--gray);
+    }
+
+    &::after {
+      content   : "";
+      position  : absolute;
+      display   : block;
+      width     : 13px;
+      height    : 2px;
+      background: var(--gray);
+      transform : rotate(90deg);
+    }
+  }
+
+  &_add {
+    margin-top   : calc(var(--bv) * 2);
+  }
+}
+
+.sound {
+  position: fixed;
+  bottom  : 0;
+  width   : 100%;
 }
 </style>
