baee003d5ee0c7d442102ec3da71d9a5a5973ab7
tnccc <irei.frontend@gmail.com>
2023-02-27 23:13:29 +0900

    feat: #38 firebase deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,8 +4,7 @@ export default {
     ssr: false,
 
     router: {
-        // base: "/pomodoroTimer/",
-        mode: 'hash'
+        base: "hash",
     },
 
     generate: {
@@ -31,37 +30,6 @@ export default {
     css: [
         "~/node_modules/ress/dist/ress.min.css", "~/assets/scss/base.scss"
     ],
-<<<<<<< Updated upstream
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    "~/node_modules/ress/dist/ress.min.css", "~/assets/scss/base.scss"
-  ],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
-=======
 
     // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
     plugins: [],
@@ -73,11 +41,8 @@ export default {
     buildModules: [],
 
     // Modules: https://go.nuxtjs.dev/config-modules
-    modules: [
-        'nuxt-svg-loader'
-    ],
+    modules: [],
 
     // Build Configuration: https://go.nuxtjs.dev/config-build
     build: {}
 }
->>>>>>> Stashed changes
